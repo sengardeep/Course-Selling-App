ab910a401f60c3decf12968c94e1640a1d42e5a6
sengardeep <sengardeep2006@gmail.com>
2025-07-28 13:55:59 +0530

    feat: add db models for user, course, admin, and purchase with mongoose schema definitions

---
+++ db.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+const ObjectId=Schema.ObjectId;
+
+const User=new Schema({
+    email : {trype: String,unique: true},
+    password : String,
+    firstName : String,
+    lastName : String
+})
+
+const Course = new Schema({
+    title : String,
+    description : String,
+    price : Number,
+    imageUrl : String,
+    creatorId : ObjectId
+})
+
+const Admin = new Schema({
+    email : {trype: String,unique: true},
+    password : String,
+    firstName : String,
+    lastName : String
+})
+
+const Purchase = new Schema({
+    userId : ObjectId,
+    courseId : ObjectId
+})
+
+const userModel=mongoose.Model('user',User);
+const courseModel=mongoose.Model('course',Course);
+const adminModel=mongoose.Model('admin',Admin);
+const purchaseModel=mongoose.Model('purchase',Purchase);
+
+module.exports={
+    userModel : userModel,
+    courseModel : courseModel,
+    adminModel : adminModel,
+    purchaseModel : purchaseModel
+}
